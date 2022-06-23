7fdc0e7c42b74a76f1ae7eac453086c7f4144f6d
Dark1106 <73903126+Duncan1106@users.noreply.github.com>
2022-06-23 12:02:40 +0200

    Update startSortDownloadsFolder.bat

--- startSortDownloadsFolder.bat
+++ startSortDownloadsFolder.bat
@@ -3,3 +3,4 @@ set ThisScriptsDir=%~dp0
 set PowerShellGUIPath=%ThisScriptsDir%SortDownloadsFolder.ps1
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 PowerShell.exe -ExecutionPolicy Unrestricted -command "&'%PowerShellGUIPath%' '%ThisScriptsDir%'"
+exit
