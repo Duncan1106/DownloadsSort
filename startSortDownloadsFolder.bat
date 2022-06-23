aa60621242b47fcee166296e50932fac172060c4
Dark1106 <73903126+Duncan1106@users.noreply.github.com>
2022-06-23 12:01:36 +0200

    Update startSortDownloadsFolder.bat
    updated for name  change of main script

--- startSortDownloadsFolder.bat
+++ startSortDownloadsFolder.bat
@@ -1,5 +1,5 @@
 @echo off
 set ThisScriptsDir=%~dp0
-set PowerShellGUIPath=%ThisScriptsDir%CLI_GUI.ps1
+set PowerShellGUIPath=%ThisScriptsDir%SortDownloadsFolder.ps1
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 PowerShell.exe -ExecutionPolicy Unrestricted -command "&'%PowerShellGUIPath%' '%ThisScriptsDir%'"
